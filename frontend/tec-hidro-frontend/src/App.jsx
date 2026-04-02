99048866a71a0199f56bc64bc677303af58555b0
CrozCamel <carlylecamelo@gmail.com>
2026-04-02 19:49:26 -0300

    atualização da tela de dados

--- frontend/tec-hidro-frontend/src/App.jsx
+++ frontend/tec-hidro-frontend/src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [entryDate, setEntryDate] = useState('')
+  const [releaseDate, setReleaseDate] = useState('')
 
   const baseUrl = 'https://tec-hidro.onrender.com/api/persons'
 
@@ -28,13 +30,19 @@ const App = () => {
   return (
     <div>
       <h1>Tec-Hidro - Gerenciador</h1>
-      
+
       <form onSubmit={addPerson} style={{ marginBottom: '20px', border: '1px solid #555', padding: '15px' }}>
         <h3>Novo Registro</h3>
         <div>Nome: <input value={newName} onChange={(e) => setNewName(e.target.value)} /></div>
         <br />
         <div>Número: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
         <br />
+        <div>
+          Data de Entrada: <input value={entryDate} onChange={(e) => setEntryDate(e.target.value)} />
+        </div>
+        <div>
+          Data de Saida: <input value={releaseDate} onChange={(e) => setReleaseDate(e.target.value)} />
+        </div>
         <button type="submit">Salvar no MongoDB</button>
       </form>
 
