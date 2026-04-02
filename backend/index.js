a11aa4502f838b4637cd89edd9587ef6affc90b7
CrozCamel <carlylecamelo@gmail.com>
2026-04-02 18:36:56 -0300

    Primeira versão do Tec-Hidro

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const url = process.env.MONGODB_URI
+console.log('conectando a', url)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+    .then(result => { console.log('conectado ao MongoDB') })
+    .catch((error) => { console.log('erro ao conectar ao MongoDB:', error.message) })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+personSchema.set('toJSON', {
+    transform:(document, returnedObject)=> {
+        returnedObject.id = returnedObject._id.toString
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+app.get('/api/persons', (req, res)=> {
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
+})
+
+app.post('/api/persons', (req, res)=>{
+    const body = req.body
+
+    if(!body.name || !body.number){
+        return res.result(400).json({error: 'Nome ou número faltando'})
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
---
+++ frontend/tec-hidro-frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const baseUrl = 'http://localhost:3001/api/persons'
+
+  useEffect(() => {
+    axios.get(baseUrl).then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = { name: newName, number: newNumber }
+
+    axios.post(baseUrl, personObject).then(response => {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
+  }
+
+  return (
+    <div>
+      <h1>Tec-Hidro - Gerenciador</h1>
+      
+      <form onSubmit={addPerson} style={{ marginBottom: '20px', border: '1px solid #555', padding: '15px' }}>
+        <h3>Novo Registro</h3>
+        <div>Nome: <input value={newName} onChange={(e) => setNewName(e.target.value)} /></div>
+        <br />
+        <div>Número: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+        <br />
+        <button type="submit">Salvar no MongoDB</button>
+      </form>
+
+      <h2>Registros no Banco de Dados:</h2>
+      <ul>
+        {persons.map(person => (
+          <li key={person.id}>{person.name}: {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
